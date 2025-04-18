0c2ccdb71727947bc94cdfc31a01d8c1df579d7d
Adrien Degand <adrien.degand@hotmail.com>
2025-04-18 16:01:37 +0200

    Add component-scan

--- library/src/main/fr/baretto/library/service/BookServiceImpl.java
+++ library/src/main/fr/baretto/library/service/BookServiceImpl.java
@@ -3,13 +3,20 @@ package fr.baretto.library.service;
 import fr.baretto.library.repository.BookRepository;
 import fr.baretto.library.entity.Book;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
+
     private BookRepository bookRepository;
+
+    @Autowired
+    public  BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
