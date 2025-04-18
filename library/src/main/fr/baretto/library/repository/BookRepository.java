5f13084588ddb4d602782fadd0c6c79889eba84a
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2025-04-18 15:42:06 +0200

    ajout annotations

--- library/src/main/fr/baretto/library/repository/BookRepository.java
+++ library/src/main/fr/baretto/library/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package fr.baretto.library.repository;
 
 import fr.baretto.library.entity.Book;
-
 import java.util.List;
 
 public interface BookRepository {
@@ -9,5 +8,4 @@ public interface BookRepository {
     void save(Book book);
 
     List<Book> findAll();
-
 }
