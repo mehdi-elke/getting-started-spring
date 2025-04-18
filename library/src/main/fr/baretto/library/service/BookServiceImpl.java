c5903a74d91dab79957c3d5d90fa7bc898773cb5
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2025-04-18 15:00:10 +0200

    Creation of BookServiceImpl

--- library/src/main/fr/baretto/library/service/BookServiceImpl.java
+++ library/src/main/fr/baretto/library/service/BookServiceImpl.java
@@ -1,4 +1,24 @@
 package fr.baretto.library.service;
 
-public class BookServiceImpl {
+import fr.baretto.library.entity.Book;
+import fr.baretto.library.repository.BookRepository;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void addBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> listBooks() {
+        return bookRepository.findAll();
+    }
 }
