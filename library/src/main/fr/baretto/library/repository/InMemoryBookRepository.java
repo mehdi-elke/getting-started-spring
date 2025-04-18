c4c5c9b200f1c5f65c0d7543b21cd5eb02c59855
Kylian <libraaa201@gmail.com>
2025-04-18 14:57:08 +0200

    Add InMemoryBookRepository

--- library/src/main/fr/baretto/library/repository/InMemoryBookRepository.java
+++ library/src/main/fr/baretto/library/repository/InMemoryBookRepository.java
@@ -1,4 +1,21 @@
 package fr.baretto.library.repository;
 
-public class InMemoryBookRepository {
+import fr.baretto.library.entity.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryBookRepository implements BookRepository {
+
+    private final List<Book> books = new ArrayList<>();
+
+    @Override
+    public void save(Book book) {
+        this.books.add(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return books;
+    }
 }
