ccb93f8bdbebf8198e1668bd56b4d7f0f54d55d8
Kylian <libraaa201@gmail.com>
2025-04-29 16:07:35 +0200

    Added getProduct on ProductService.java

--- catalog/src/main/java/fr/baretto/service/ProductService.java
+++ catalog/src/main/java/fr/baretto/service/ProductService.java
@@ -1,4 +1,24 @@
 package fr.baretto.service;
 
+import fr.baretto.data.Product;
+import fr.baretto.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product getProduct(UUID id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.orElse(null);
+    }
 }
