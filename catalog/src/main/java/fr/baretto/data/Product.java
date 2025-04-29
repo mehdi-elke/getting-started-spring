f2c51509ba659ef6e6c897c155b7717459d2efdf
Kylian <libraaa201@gmail.com>
2025-04-29 15:40:30 +0200

    Added ProductRepository

--- catalog/src/main/java/fr/baretto/data/Product.java
+++ catalog/src/main/java/fr/baretto/data/Product.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 @Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     @Column(nullable = false)
     private String name;
