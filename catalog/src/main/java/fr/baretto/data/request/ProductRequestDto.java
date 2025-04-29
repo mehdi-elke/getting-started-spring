a5b52ba80931be270391519b792a98b318a31c94
Adrien Degand <adrien.degand@hotmail.com>
2025-04-29 16:17:10 +0200

    CRUD: updateProduct

--- catalog/src/main/java/fr/baretto/data/request/ProductRequestDto.java
+++ catalog/src/main/java/fr/baretto/data/request/ProductRequestDto.java
@@ -7,5 +7,34 @@ public class ProductRequestDto {
     private String name;
     private String description;
     private Double price;
-    
+
+    public ProductRequestDto(String name, String description, Double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
