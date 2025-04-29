0afadff1ac6e2fa6c4032610629419728cee39c6
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2025-04-29 15:35:17 +0200

    Product Class

--- catalog/src/main/java/fr/baretto/data/Product.java
+++ catalog/src/main/java/fr/baretto/data/Product.java
@@ -1,4 +1,49 @@
 package fr.baretto.data;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    @Column(nullable = false)
+    private Double price;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
