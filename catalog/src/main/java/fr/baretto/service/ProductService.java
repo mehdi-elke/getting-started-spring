f647ff1aa4821e868544b9ae7242f0b464a78bde
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2025-04-29 16:05:41 +0200

    CRUD : deleteProduct

--- catalog/src/main/java/fr/baretto/service/ProductService.java
+++ catalog/src/main/java/fr/baretto/service/ProductService.java
@@ -1,4 +1,26 @@
 package fr.baretto.service;
 
+import fr.baretto.data.Product;
+import fr.baretto.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public boolean deleteProduct(UUID productId){
+        try {
+            productRepository.deleteById(productId);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+
+    }
 }
